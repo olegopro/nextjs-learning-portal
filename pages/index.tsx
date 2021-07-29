fcd0521c4b519432699ad9d0186afee54ca79796
olegopro <evilgazz@gmail.com>
2021-07-29 18:28:38 +0300

    Сделал компонент Tag.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Htag, P } from '../components'
+import { Button, Htag, P, Tag } from '../components'
 
 export default function Home(): JSX.Element {
 	return (
@@ -14,6 +14,11 @@ export default function Home(): JSX.Element {
 			<P size="l">Большой</P>
 			<P>Средний</P>
 			<P size="l">Маленький</P>
+			<Tag size="s">Small</Tag>
+			<Tag size="m">Medium</Tag>
+			<Tag size="s" color="green">
+				Small green
+			</Tag>
 		</>
 	)
 }
