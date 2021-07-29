4f092349efca61332d808605dc4486a75e46e713
olegopro <evilgazz@gmail.com>
2021-07-29 12:14:21 +0300

    Сделал компонент Header динамическим.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
-import Image from 'next/image'
+import React from 'react'
+import { Htag } from '../components'
 
 export default function Home(): JSX.Element {
-	return <div> </div>
+	return (
+		<div>
+			<Htag tag="h1">Текст</Htag>
+		</div>
+	)
 }
