240b6a2b5310b0c6b18a09e8c32e8f363aebaca4
olegopro <evilgazz@gmail.com>
2021-07-29 11:14:35 +0300

    Подключил шрифты. Настроил базовые переменные в css.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../styles/globals.css'
 
+import Head from 'next/head'
+import React from 'react'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>MyTop - наш лучший топ!</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
