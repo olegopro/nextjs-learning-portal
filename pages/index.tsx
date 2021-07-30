36a9ae5c539e29ebeb2e8554f7be4a31a673b20d
olegopro <evilgazz@gmail.com>
2021-07-30 11:28:14 +0300

    Сделал Layout и Header, Footer, Sidebar.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { Button, Htag, P, Tag } from '../components'
+import React, { useState } from 'react'
+import { Button, Htag, P, Rating, Tag } from '../components'
+import { Layout } from '../layout/Layout'
 
 export default function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4)
 	return (
-		<>
+		<Layout>
 			<Htag tag="h1">Текст</Htag>
 			<Button appearance="primary" arrow="right">
 				Кнопка
@@ -19,6 +21,7 @@ export default function Home(): JSX.Element {
 			<Tag size="s" color="green">
 				Small green
 			</Tag>
-		</>
+			<Rating rating={rating} isEditable setRating={setRating} />
+		</Layout>
 	)
 }
