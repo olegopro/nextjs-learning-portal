9cb945c83d15cd66654a399527dc47dafcd7d2cb
olegopro <evilgazz@gmail.com>
2021-08-02 19:36:53 +0300

    Сделал получение данных меню через api.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import React, { useState } from 'react'
 import { Button, Htag, P, Rating, Tag } from '../components'
 import { withLayout } from '../layout/Layout'
 import axios from 'axios'
+import { MenuItem } from '../interfaces/menu.interfaces'
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(4)
 
 	return (
@@ -25,16 +26,28 @@ function Home(): JSX.Element {
 				Small green
 			</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
+			<ul>
+				{menu.map(m => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	)
 }
 
 export default withLayout(Home)
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
 	const firstCategory = 0
-	const { data: menu } = await axios.post(process.env.NEXT_PUBPLIC_DOMAIN + '/api/top-page/find')
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBPLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	})
 	return {
 		props: { menu, firstCategory }
 	}
+}
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[]
+	firstCategory: number
 }
