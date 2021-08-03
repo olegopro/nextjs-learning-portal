8035cf18cb3936727d6c82fdba60266f99f91f44
olegopro <evilgazz@gmail.com>
2021-08-03 18:25:31 +0300

    Верстка sidebar.

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,13 @@
 import { Menu } from '../Menu/Menu'
 import { SidebarProps } from './Sidebar.props'
+import Logo from '../logo.svg'
+import cn from 'classnames'
+import styles from './Sidebar.module.css'
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
+		<div className={cn(className, styles.sidebar)} {...props}>
+			<Logo className={styles.logo} />
 			<Menu />
 		</div>
 	)
