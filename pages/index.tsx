e3537bdee65e9d7d33121e153419c8040f0a2014
olegopro <evilgazz@gmail.com>
2021-08-03 14:24:45 +0300

    Сделал верстку меню.

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,11 +26,6 @@ function Home({ menu }: HomeProps): JSX.Element {
 				Small green
 			</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
-			<ul>
-				{menu.map(m => (
-					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
-				))}
-			</ul>
 		</>
 	)
 }
@@ -39,7 +34,7 @@ export default withLayout(Home)
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
 	const firstCategory = 0
-	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBPLIC_DOMAIN + '/api/top-page/find', {
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
 		firstCategory
 	})
 	return {
