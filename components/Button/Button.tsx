395b105c8332adf0945c53b4bf8b204b751dd0cd
olegopro <evilgazz@gmail.com>
2021-08-06 19:25:27 +0300

    Сделал возможность тапать по меню с клавиатуры.

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,11 +1,14 @@
 import styles from './Button.module.css'
 import { ButtonProps } from './Button.props'
-import cs from 'classnames'
 import ArrowIcon from './arrow.svg'
-export const Button = ({ appearance, children, className, arrow = 'none', ...props }: ButtonProps): JSX.Element => {
+import cn from 'classnames'
+import { motion } from 'framer-motion'
+
+export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
 	return (
-		<button
-			className={cs(styles.button, className, {
+		<motion.button
+			whileHover={{ scale: 1.05 }}
+			className={cn(styles.button, className, {
 				[styles.primary]: appearance == 'primary',
 				[styles.ghost]: appearance == 'ghost'
 			})}
@@ -13,10 +16,14 @@ export const Button = ({ appearance, children, className, arrow = 'none', ...pro
 		>
 			{children}
 			{arrow != 'none' && (
-				<span className={cs(styles.arrow, { [styles.down]: arrow == 'down' })}>
+				<span
+					className={cn(styles.arrow, {
+						[styles.down]: arrow == 'down'
+					})}
+				>
 					<ArrowIcon />
 				</span>
 			)}
-		</button>
+		</motion.button>
 	)
 }
